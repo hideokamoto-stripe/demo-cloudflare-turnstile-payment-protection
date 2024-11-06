cf401993db2b295d0bb2ccf78c137d68c852bc4a
Hidetaka Okamoto <hideokamoto@stripe.com>
2024-11-06 16:30:40 +0900

    form

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,19 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono<{ Bindings: CloudflareBindings }>()
+const app = new Hono<{
+  Bindings: CloudflareBindings;
+}>();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.html(
+    <main>
+      <form id="payment-form">
+        <div id="payment-element"></div>
+        <div id="result"></div>
+        <button type="submit">Order</button>
+      </form>
+    </main>,
+  );
+});
 
-export default app
+export default app;
