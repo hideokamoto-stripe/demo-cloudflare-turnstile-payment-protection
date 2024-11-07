fe89c324a43c5a04ce745ae7b568a00c33c76d94
Hidetaka Okamoto <hideokamoto@stripe.com>
2024-11-07 15:21:00 +0900

    server side confirmation

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,69 @@ import { Hono } from "hono";
 import { env } from "hono/adapter";
 import { html } from "hono/html";
 import Stripe from "stripe";
+import { HTTPException } from "hono/http-exception";
 
 const app = new Hono<{
   Bindings: CloudflareBindings;
 }>();
+
+type TurnstileResult = {
+  success: boolean;
+  challenge_ts: string;
+  hostname: string;
+  "error-codes": Array<string>;
+  action: string;
+  cdata: string;
+};
+
+app.post("/pre-confirm", async (c) => {
+  const { TURNSTILE_SECRET_KEY, STRIPE_SECRET_KEY } = env(c);
+  const stripe = new Stripe(STRIPE_SECRET_KEY, {
+    apiVersion: "2024-10-28.acacia",
+    appInfo: {
+      name: "example/cloudflare-turnstile",
+    },
+  });
+
+  const body = await c.req.json();
+  const ip = c.req.header("CF-Connecting-IP");
+  const paymentIntentId = body.payment_intent_id
+
+  const formData = new FormData();
+  formData.append("secret", TURNSTILE_SECRET_KEY);
+  formData.append("response", body.turnstile_token);
+  formData.append("remoteip", ip || "");
+  const turnstileResult = await fetch(
+    "https://challenges.cloudflare.com/turnstile/v0/siteverify",
+    {
+      body: formData,
+      method: "POST",
+    },
+  );
+  const outcome = await turnstileResult.json<TurnstileResult>();
+
+  await stripe.paymentIntents.update(paymentIntentId, {
+    metadata: {
+      turnstile_result: outcome.success ? 'success' : 'failed',
+      turnstile_challenge_ts: outcome.challenge_ts,
+    }
+  })
+
+  if (!outcome.success) {
+    throw new HTTPException(401, {
+      res: new Response(
+        JSON.stringify({
+          success: outcome.success,
+          message: "Unauthorized",
+          error_codes: outcome["error-codes"],
+        }),
+      ),
+    });
+  }
+  return c.json({
+    success: outcome.success
+  });
+});
 
 app.get("/", async (c) => {
   const { TURNSTILE_SITE_KEY, STRIPE_SECRET_KEY, STRIPE_PUBLISHABLE_KEY } =
@@ -26,9 +85,7 @@ app.get("/", async (c) => {
         <div id="payment-element"></div>
         <div id="result"></div>
         <div class="cf-turnstile"></div>
-        <button type="submit" disabled>
-          Order
-        </button>
+        <button type="submit">Order</button>
       </form>
       {html`
         <script src="https://js.stripe.com/v3/"></script>
@@ -39,6 +96,8 @@ app.get("/", async (c) => {
         <script>
           let turnstileToken = "";
           const submitButon = document.querySelector("button[type='submit']");
+          const resultElement = document.getElementById("result");
+
           function _turnstileCb() {
             turnstile.render(".cf-turnstile", {
               sitekey: "${TURNSTILE_SITE_KEY}",
@@ -68,6 +127,31 @@ app.get("/", async (c) => {
             if (submitButon) {
               submitButon.setAttribute("disabled", true);
             }
+            resultElement.innerHTML = "";
+
+            const preConfirmationResponse = await fetch("/pre-confirm", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                turnstile_token: turnstileToken,
+                payment_intent_id: "${paymentIntent.id}",
+              }),
+            });
+            const preConfirmationResult = await preConfirmationResponse.json();
+            console.log(preConfirmationResult);
+            if (!preConfirmationResult.success) {
+              submitButon.removeAttribute("disabled");
+              resultElement.innerHTML = JSON.stringify(
+                preConfirmationResult,
+                null,
+                2,
+              );
+              return;
+            }
+            return;
+
             const { error: submitError } = await elements.submit();
             if (submitError) {
               console.log(submitError);
